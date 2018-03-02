67a31f10e9c486dd274b1a06e680a02333386b19
Kenton Wang <smartepsh@gmail.com>
2018-03-02 18:24:17 +0800

    temp

--- lib/kenton_flow.ex
+++ lib/kenton_flow.ex
@@ -9,4 +9,28 @@ defmodule KentonFlow do
     Server.stop()
     Plot.create(file_name, @scopes)
   end
+
+  def test do
+    data = [
+      {"elixir", 1_000},
+      {"erlang", 60_000},
+      {"concurrency", 3_200_000},
+      {"elixir", 4_000_000},
+      {"erlang", 5_000_000},
+      {"erlang", 6_000_000},
+      {"elixir", 0}
+    ]
+
+    window =
+      Flow.Window.fixed(1, :hour, fn {_word, time} -> time end)
+      |> Flow.Window.allowed_lateness(1, :millisecond)
+
+    data
+    |> Flow.from_enumerable()
+    |> Flow.partition(window: window, stages: 1, max_demand: 5)
+    |> Flow.reduce(fn -> %{} end, fn {word, _}, acc ->
+      Process.sleep(100)
+      Map.update(acc, word, 1, &(&1 + 1))
+    end)
+  end
 end
