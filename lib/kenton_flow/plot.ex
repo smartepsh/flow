d48889a3040e203bd68a992778ce2458630b5ecb
Kenton Wang <smartepsh@gmail.com>
2018-03-02 13:21:01 +0800

    feat: add num and string data sources

--- lib/kenton_flow/plot.ex
+++ lib/kenton_flow/plot.ex
@@ -1,10 +1,16 @@
 defmodule KentonFlow.Plot do
-  def write_config_file(file_name, output_file, time_range, scopes) do
+  def create(file_name, scopes) do
+    time_range = get_time_range()
+    write_config_file(file_name, scopes, time_range)
+    gen_png(file_name)
+  end
+
+  defp write_config_file(file_name, scopes, time_range) do
     f = File.open!("#{file_name}.gp", [:write])
 
     IO.write(
       f,
-      "set terminal png font \"Arial,10\" size 700,500\nset output \"#{output_file}.png\"\nset title \"Elixir Flow processing progress over time\"\nset xlabel \"Time (ms)\"set ylabel \"Items processed\"set key top left\nset xrange [0:#{
+      "set terminal png font \"Arial,10\" size 700,500\nset output \"#{file_name}.png\"\nset title \"Elixir Flow processing progress over time\"\nset xlabel \"Time (ms)\"set ylabel \"Items processed\"set key top left\nset xrange [0:#{
         time_range
       }]\nplot #{set_plots(scopes)}"
     )
@@ -12,7 +18,7 @@ defmodule KentonFlow.Plot do
     File.close(f)
   end
 
-  def gen_png(file_name) do
+  defp gen_png(file_name) do
     System.cmd("gnuplot", ["#{file_name}.gp"])
   end
 
@@ -27,5 +33,10 @@ defmodule KentonFlow.Plot do
       end)
 
     plots
+  end
+
+  defp get_time_range do
+    {line, _} = System.cmd("tail", ["-n 1", "final.log"])
+    line |> String.split("\t") |> List.first() |> Kernel.+(500)
   end
 end
---
+++ lib/kenton_flow/sources.ex
@@ -0,0 +1,20 @@
+defmodule KentonFlow.Sources do
+  alias KentonFlow.Server
+
+  def source(delay \\ 50, count \\ 1000, length \\ 8) do
+    Stream.resource(
+      fn -> 0 end,
+      fn num ->
+        if num > count do
+          {:halt, num}
+        else
+          string = Enum.take_random(?a..?z, length) |> List.to_string()
+          Process.sleep(delay)
+          Server.incr(:load_data)
+          {[%{num: num, value: string}], num + 1}
+        end
+      end,
+      fn _ -> [] end
+    )
+  end
+end
